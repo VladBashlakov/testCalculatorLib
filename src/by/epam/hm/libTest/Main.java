b4d320e76c6a9d460eda486357034cbbcdca5dd5
Vlad <VladBashlakov@gmail.com>
2022-02-28 03:02:38 +0300

    Task 02 Java Fundamentals 02 jar-library (part2)

---
+++ src/by/epam/hm/libTest/Main.java
@@ -0,0 +1,10 @@
+package by.epam.hm.libTest;
+
+import by.epam.hm.view.CalcView;
+
+public class Main {
+
+    public static void main(String[] args) {
+        CalcView.printResultOfCalculation("2+2*2");
+    }
+}
